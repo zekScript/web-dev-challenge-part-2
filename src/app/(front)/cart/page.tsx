fb74a4647c545782c2c626ae22969d562bce575f
zekScript <armandasdarbinis@gmail.com>
2025-02-27 12:51:55 +0200

    Added product id work and cart

--- src/app/(front)/cart/page.tsx
+++ src/app/(front)/cart/page.tsx
@@ -1,16 +1,34 @@
 
 import { prisma } from "@/lib/db"
-const Cart: React.FC = () => {
+import Link from "next/link"
+const Cart: React.FC = async () => {
 
 
-const products = prisma.
+const productLists = await prisma.product.findMany()
+
 
     return(
         <>
-        
         <h1>Your cart</h1>
 
+        {productLists.map((productList) => (
+            
+            <div key={productList.id} className="w-full h-full ">
+                <div>
+                    <Link href={`/products/${productList.id}`}>
+                    <h1>Order by:        {/* Get current user */}                    </h1>
+                    <p>Automobilio name: {productList.productName}</p>
+                    <p>Automobilio modelis: {productList.productModel}</p>
+                    <p>Automobilio aprašymas: {productList.aboutProduct}</p>
+                    {/* <p>užsakymo Data: {productList.orderDate as string}</p> */}
+                    </Link>
+                    
 
+
+
+                </div>
+            </div>
+        ))}
         
         </>
     )
---
+++ src/app/(front)/products/[productsID]/page.tsx
@@ -0,0 +1,22 @@
+
+import {prisma} from "@/lib/db"
+
+export default async function ProductPage(){
+
+    
+
+    return(
+        <>
+        
+        <h1>Hello product</h1>
+        
+        </>
+    )
+}
+
+
+
+
+
+
+
